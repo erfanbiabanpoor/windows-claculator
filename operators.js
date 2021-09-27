ee7cb993f4d3a2edf920ff7921ffd5346ad70998
erfan <erfanbiabanpoor@yahoo.com>
2021-09-27 14:39:46 +0330

    wirting

--- operators.js
+++ operators.js
@@ -1,24 +1,23 @@
-export const plus =()=>{
-    for(let inputValue of Array) {
-        inputValue.result = +inputValue.num_1 + +inputValue.num_2;
-    }
-}
+export const plus = () => {
+  for (let inputValue of Array) {
+    inputValue.result = +inputValue.num_1 + +inputValue.num_2;
+  }
+};
 
 export const minus = () => {
-    for(let inputValue of Array) {
-        inputValue.result = +inputValue.num_1 - +inputValue.num_2;
-    }
-}
+  for (let inputValue of Array) {
+    inputValue.result = +inputValue.num_1 - +inputValue.num_2;
+  }
+};
 
 export const multiplication = () => {
-    for(let inputValue of Array) {  
-        inputValue.result = +inputValue.num_1 * +inputValue.num_2;
-    }
-}
+  for (let inputValue of Array) {
+    inputValue.result = +inputValue.num_1 * +inputValue.num_2;
+  }
+};
 
 export const division = () => {
-    for(let inputValue of Array) {  
-        inputValue.result = +inputValue.num_1 / +inputValue.num_2;
-    }
-}
-
+  for (let inputValue of Array) {
+    inputValue.result = +inputValue.num_1 / +inputValue.num_2;
+  }
+};
--- sandbox.js
+++ sandbox.js
@@ -1,31 +1,77 @@
-export const input = document.getElementById('.input');
+export const input = document.getElementById(".input");
 
-export let Array=[
-    {
-       num_1 : '0',
-       num_2 : '0',
-       inQueueOperator : false ,
-       operator : null ,
-       equal : false ,
-       result : '0' ,
-       memory : false ,
-       id : 0 ,
+export let Array = [
+  {
+    num_1: "0",
+    num_2: "0",
+    inQueueOperator: false,
+    operator: null,
+    equal: false,
+    result: "0",
+    memory: false,
+    id: 0,
+  },
+];
+
+const ArrayValue = () => {
+  let object = {
+    num_1: "0",
+    num_2: "0",
+    inQueueOperator: false,
+    operator: null,
+    equal: false,
+    result: "0",
+    memory: false,
+    id: Array.length,
+  };
+  Array.push(object);
+};
+const buttonValue = () => {
+  const calculatorMain = document.querySelector("division");
+
+  calculatorMain.addEventListener("click", (event) => {
+    const eventTarget = event.target;
+    let dataBaseLength = Array.length - 1;
+
+    if (eventTarget.classList.contains("number"))
+      changeNumbers(eventTarget.value, dataBaseLength);
+    if (eventTarget.classList.contains("operator"))
+      changeOperator(eventTarget.value, dataBaseLength);
+    if (eventTarget.classList.contains("decimal"))
+      inputDecimal(eventTarget.value, dataBaseLength);
+    if (eventTarget.classList.contains("clear-entry"))
+      clearEntry(dataBaseLength);
+    if (eventTarget.classList.contains("clear-all")) clearAll(dataBaseLength);
+    if (eventTarget.classList.contains("negative"))
+      convertNumber(dataBaseLength);
+    if (eventTarget.classList.contains("back-space")) backSpace(dataBaseLength);
+    if (eventTarget.classList.contains("equal")) {
+      equal(dataBaseLength);
     }
-]
+  });
+};
+const changeNavbarStyle = () => {
+  const historyButton = document.querySelector(".historylink");
+  const memoryButton = document.querySelector(".memorylink");
 
-const ArrayValue =()=>{
-    let object = {
-        num_1 : '0',
-        num_2 : '0',
-        inQueueOperator : false ,
-        operator : null ,
-        equal : false ,
-        result : '0' ,
-        memory : false ,  
-        id : Array.length, 
-     }
-     Array.push(object);
-}
+  historyButton.addEventListener("click", (event) => {
+    showHistory(event.target);
+  });
 
+  memoryButton.addEventListener("click", (event) => {
+    showMemory(event.target);
+  });
+};
 
 
+
+
+
+
+
+
+
+
+
+changeNavbarStyle();
+buttonValue();
